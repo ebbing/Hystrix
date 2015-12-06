5a0c217ab780b81aef283002af33773545d08697
ebbing <ebbing@adesso.de>
2015-12-06 19:47:44 +0100

    Added missing license note.

--- hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/annotation/CommandKey.java
+++ hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/annotation/CommandKey.java
@@ -1,10 +1,25 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.hystrix.contrib.javanica.annotation;
 
 import java.lang.annotation.*;
 
 /**
- * This annotation specifies a parameter of a hystrix command method to use its value
- * as the hystrix command key.
+ * This annotation is used to specify a parameter of a method annotated with {@link HystrixCommand}.
+ * The parameter value will be used as the hystrix command key.
  */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
